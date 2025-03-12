5ecfb84487f98c6e2b4fe06df6fe982ee7ab4425
Eclaizr <claire.mrtn13@gmail.com>
2025-03-12 21:33:54 +0100

    ajout logins

--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-use App\Http\Controllers\AuthController;
-
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/logout', [AuthController::class, 'logout']);
-Route::get('/dashboard', function () {
-    return view('dashboard'); // Page après connexion
-})->middleware('auth'); // Sécuriser l'accès
