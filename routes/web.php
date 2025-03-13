533a1ed72d8c9a25fba95d85638db79e5addff80
RubenPL0 <ruben.preti-_-levy@telecom-sudparis.eu>
2025-03-13 20:51:24 +0100

    Version 1 de ruben

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AstroController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Protège la route avec 'auth' si nécessaire
+Route::middleware('auth')->group(function () {
+    Route::get('/affichage_astro', [AstroController::class, 'showProfile'])->name('profil');
 });
